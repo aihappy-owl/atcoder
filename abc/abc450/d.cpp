c586fb0f75e59353095e664e264431ec49494310
aihappy-owl <aihappy.owl@gmail.com>
2026-04-06 22:26:16 +0900

    WIP: abc450 d
    入力の受け取りまで完了

---
+++ abc/abc450/d.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> A(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    
+    return 0;
+}
