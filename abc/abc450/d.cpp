c89184fa5a2d9d049750a955a29623613aa91564
aihappy-owl <aihappy.owl@gmail.com>
2026-04-10 23:35:50 +0900

    AC: abc450 d

--- abc/abc450/d.cpp
+++ abc/abc450/d.cpp
@@ -6,16 +6,32 @@
 using namespace std;
 
 int main() {
-    int N, K;
+    int N, K, ans;
     cin >> N >> K;
 
-    vector<int> A(N);
+    vector<int> A(N+1);
 
     for (int i = 0; i < N; i++) {
         cin >> A[i];
+        A[i] %= K;
     }
     
-    sort(A.begin(), A.end());
+    sort(A.begin(), A.begin() + N);
+
+    A[N] = A[0] + K;
+
+    ans = A[1] - A[0];
+
+    for (int i = 1; i < N; i++) {
+        if (ans <= A[i+1] - A[i]) {
+            ans = A[i+1] - A[i];
+        }
+        
+    }
+
+    ans = K - ans;
+
+    cout << ans << "\n";
 
     return 0;
 }
