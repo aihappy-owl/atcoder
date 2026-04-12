23c285880753db4019e008a0d42d631b5aa9496e
aihappy-owl <aihappy.owl@gmail.com>
2026-04-12 15:21:50 +0900

    WIP: abc450 e
    文字列の長さの配列まで完了

--- abc/abc450/e.cpp
+++ abc/abc450/e.cpp
@@ -11,12 +11,21 @@ int main() {
 
     cin >> X >> Y >> Q;
 
-    vector<long long> L(Q), R(Q);
+    vector<long long> L(Q), R(Q), S, S_C;
     vector<string> C(Q);
 
     for (int i = 0; i < Q; i++) {
         cin >> L[i] >> R[i] >> C[i];
     }
 
+    S.push_back(X.size());
+    S.push_back(Y.size());
+
+    while (S.back() <= 1000000000000000000LL) {
+        S.push_back(S[S.size() - 1] + S[S.size() - 2]);
+    }
+
+    cout << S.back() << "\n";
+    
     return 0;
 }
