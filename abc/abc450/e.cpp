5d7644939903e4a9795aee3c0c74fc32df8f6fe3
aihappy-owl <aihappy.owl@gmail.com>
2026-04-17 23:48:56 +0900

    AC: abc450 e

--- abc/abc450/e.cpp
+++ abc/abc450/e.cpp
@@ -4,28 +4,74 @@
 #include <algorithm>
 
 using namespace std;
+using ll = long long;
+
+string X, Y;
+ll Q, X_count, Y_count;
+
+vector<ll> S, S_C;
+ll L, R;
+char C;
+
+void solve(char C, int k, ll m, vector<ll> &S) {
+    if (m == 0) return;
+    
+    if (k == 0) {
+        X_count += count(X.begin(), X.begin() + m, C);
+        return;
+    }
+
+    if (k == 1) {
+        Y_count += count(Y.begin(), Y.begin() + m, C);
+        return;
+    }
+
+    if (m <= S[k - 1]) {
+        solve(C, k - 1, m, S);
+        solve(C, k - 2, 0, S);
+    }
+
+    if (m > S[k - 1]) {
+        X_count += S_C[k - 1];
+        solve(C, k - 2, m - S[k - 1], S);
+    }
+
+    return;
+}
 
 int main() {
-    string X, Y;
-    int Q;
-
     cin >> X >> Y >> Q;
-
-    vector<long long> L(Q), R(Q), S, S_C;
-    vector<string> C(Q);
-
-    for (int i = 0; i < Q; i++) {
-        cin >> L[i] >> R[i] >> C[i];
-    }
 
     S.push_back(X.size());
     S.push_back(Y.size());
 
-    while (S.back() <= 1000000000000000000LL) {
-        S.push_back(S[S.size() - 1] + S[S.size() - 2]);
+    for (int i = 0; i < Q; i++) {
+        X_count = Y_count = 0;
+        S = S_C = {};
+
+        cin >> L >> R >> C;
+
+        S.push_back(X.size());
+        S.push_back(Y.size());
+
+        S_C.push_back(count(X.begin(), X.end(), C));
+        S_C.push_back(count(Y.begin(), Y.end(), C));
+
+        while (S.back() <= R) {
+            S.push_back(S[S.size() - 1] + S[S.size() - 2]);
+            S_C.push_back(S_C[S_C.size() - 1] + S_C[S_C.size() - 2]);
+        }
+        
+        solve(C, S.size(), L - 1, S);
+        ll L_count = X_count + Y_count;
+
+        X_count = Y_count = 0;
+
+        solve(C, S.size(), R, S);
+        ll R_count = X_count + Y_count;
+
+        cout << R_count - L_count << "\n";
     }
 
-    cout << S.back() << "\n";
-    
     return 0;
 }
