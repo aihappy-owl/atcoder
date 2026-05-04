05974aedca58555e5b1a92d0e74c9bdd9cc9f761
aihappy-owl <aihappy.owl@gmail.com>
2026-05-04 10:17:27 +0900

    AC: typical90 010

--- typical90/010.cpp
+++ typical90/010.cpp
@@ -6,31 +6,28 @@
 using namespace std;
 
 int main() {
-    int n, q, l, r, sum1, sum2;
+    int c, p, n, q, l, r;
     cin >> n;
 
-    vector<int> c(n, 0), p(n, 0 );
+    vector<int> sum1(n + 1, 0), sum2(n + 1, 0);
 
-    for (int i = 0; i < n; i++) {
-        cin >> c[i] >> p[i];
+    for (int i = 1; i <= n; i++) {
+        cin >> c >> p;
+        if (c == 1) {
+            sum1[i] = sum1[i - 1] + p;
+            sum2[i] = sum2[i - 1];
+        } else if (c == 2) {
+            sum2[i] = sum2[i - 1] + p;
+            sum1[i] = sum1[i - 1];
+        }
     }
 
     cin >> q;
 
     for (int i = 0; i < q; i++) {
-        sum1 = sum2 = 0;
-
         cin >> l >> r;
 
-        for (int i = l - 1; i < r; i++) {
-            if (c[i] == 1) {
-                sum1 += p[i];
-            } else if (c[i] == 2) {
-                sum2 += p[i];
-            }
-        }
-
-        cout << sum1 << " " << sum2 << "\n";
+        cout << sum1[r] - sum1[l - 1] << " " << sum2[r] - sum2[l - 1] << "\n";
     }
     
     return 0;
